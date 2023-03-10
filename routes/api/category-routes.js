12234bfe69439fcfc6229dd22b9424f23c78077b
Israel Brassell <israel.brassell@gmail.com>
2023-03-10 12:44:45 -0500

    Category Routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,75 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/api/categories', async (req, res) => {
+  try {
+    const categories = await Category.findAll({
+      include: [
+        {
+          model: Product,
+          as: 'products' // the alias for the association between Category and Product
+        }
+      ]
+    });
+
+    res.json(categories);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/api/categories:id', async (req, res) => {
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [
+        {
+          model: Product,
+          as: 'products' // the alias for the association between Category and Product
+        }
+      ]
+    });
+
+    res.json(category);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/api/categories', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name 
+  })
+  .then(category => {
+    res.status(200).json(category);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/api/categories:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then (() => {
+    console.log('Item has been deleted from Categories')
+    res.status(204);
+  })
+  .catch (err => {
+    console.error(err)
+    res.status(500).json(err)
+  });
 });
 
 module.exports = router;
